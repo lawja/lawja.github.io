54abe3a58aca32c3b2a9b6cd9eebc35f7b4a18a2
Jake Lawrence <lawja@mac.lan>
2026-02-05 21:04:46 -0500

    updating contents

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$('.pane-entry').on('mouseenter', function(event){
-    let width = parseInt($(this.children[0]).css('width').split('px')[0]) + 10;
-    $(this.children[1]).css('width', width);
-});
-
-$('.pane-entry').on('mouseleave', function(event){
-    $(this.children[1]).css('width', '0px');
-});
